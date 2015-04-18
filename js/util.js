cee3f2febb34c58a230ea52386afbdafacb297a5
dupeng <dupengdl@gmail.com>
2015-04-18 17:33:56 +0800

    add get url param util

--- js/util.js
+++ js/util.js
@@ -38,3 +38,24 @@ function shuffle(arr) {
     }
     return tmparr;
 }
+//获取url参数
+var $_GET = (function(){
+    var url = window.document.location.href.toString();
+    var u = url.split("?");
+    if(typeof(u[1]) == "string"){
+        if(u[1].indexOf('#') != -1) {
+            u = u[1].split("#");
+            u = u[0].split("&");
+        } else {
+            u = u[1].split("&");
+        }
+        var get = {};
+        for(var i in u){
+            var j = u[i].split("=");
+            get[j[0]] = j[1];
+        }
+        return get;
+    } else {
+        return {};
+    }
+})();
