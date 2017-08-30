9bdc7e35e316d3ad186d0c9ff054c0a807ef9d25
dupengdl <dupengdl@gmail.com>
2017-08-31 01:00:20 +0800

    test buffer

---
+++ node/buffer-index.js
@@ -0,0 +1,5 @@
+var buf = new Buffer(100);
+console.log(buf[1]);
+buf[10]=110;
+console.log(buf[10]);
+console.log(buf);
---
+++ node/buffer-length.js
@@ -0,0 +1,4 @@
+var zhbuffer = new Buffer('深', 'utf-8');
+var enbuffer = new Buffer('s');
+console.log(zhbuffer);
+console.log(enbuffer);
