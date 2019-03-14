9a34fbafe70fb86a876b78af6b8a8659bfcf7848
dupengdl <dupengdl@gmail.com>
2019-03-14 20:48:22 +0800

    chore(config): add cz config file

---
+++ cz-config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  types: {
+    "WIP": {
+      "description": "💪  WIP:      Work in progress",
+      "title": "WIP"
+    },
+    "feat": {
+      "description": "✨  feat:     A new feature",
+      "title": "Features"
+    },
+    "fix": {
+      "description": "🐞  fix:      A bug fix",
+      "title": "Bug Fixes"
+    },
+    "docs": {
+      "description": "📚  docs:     Documentation only changes",
+      "title": "Documentation"
+    },
+    "style": {
+      "description": "💅  style:    Code Style, Changes that do not affect the meaning of the code (white-space, formatting, etc)",
+      "title": "Styles"
+    },
+    "refactor": {
+      "description": "🛠  refactor: A code change that neither fixes a bug nor adds a feature",
+      "title": "Code Refactoring"
+    },
+    "perf": {
+      "description": "⏱  perf:     A code change that improves performance",
+      "title": "Performance Improvements"
+    },
+    "test": {
+      "description": "🏁  test:     Add missing tests or correcting existing tests",
+      "title": "Tests"
+    },
+    "build": {
+      "description": "📦  build:    Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)",
+      "title": "Builds"
+    },
+    "chore": {
+      "description": "🗯  chore:    Changes that don't modify src or test files. Such as updating build tasks, package manager",
+      "title": "Chores"
+    },
+    "revert": {
+      "description": "⏪  revert:   Revert to a commit",
+      "title": "Reverts"
+    }
+  }
+};
