e885eee323d4880000e59e4cd602ccaca12e2e9c
dupengdl <dupengdl@gmail.com>
2020-05-27 15:30:31 +0800

    improvement: update fibonacci

--- snippets/fibonacci.js
+++ snippets/fibonacci.js
@@ -25,6 +25,35 @@ console.time('fib ite time')
 console.log(fibIte(40))
 console.timeEnd('fib ite time')
 
+var cacheObj = {1:1, 2:1}
+function fibCache(n) {
+  if (!cacheObj[n-1]) {
+    cacheObj[n-1] = fibCache(n-1)
+  }
+  if (!cacheObj[n-2]) {
+    cacheObj[n-2] = fibCache(n-2)
+  }
+  return cacheObj[n-1] + cacheObj[n-2]
+}
+
+console.time('fib cache time')
+console.log(fibCache(40))
+console.timeEnd('fib cache time')
+
+function fibDP(n) {
+  const dp=[]
+  dp[0]=0;
+  dp[1]=dp[2]=1;
+  for(let i=3;i<=n;i++){
+    dp[i]=dp[i-1]+dp[i-2]
+  }
+  return dp[n]
+}
+
+console.time('fib dp time')
+console.log(fibDP(40))
+console.timeEnd('fib dp time')
+
 function fibTail(n, first, second) {
   if (n === 1) return first
   if (n === 2) return second
